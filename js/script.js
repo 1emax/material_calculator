29594213637b17c4526e32dd26f516cbbfcf10fa
1emax <1emax@mail.ru>
2015-02-04 05:30:35 +0200

    1 view of calc

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-  $(function() {
-  	$( "#radio" ).buttonset();
-  	$( "input[type=submit]" )
+$(function() {
+	//radio button view
+	$( "#radio" ).buttonset();
+
+	//button view  	
+	$( "input[type=submit]" )
       .button()
       .click(function( event ) {
         event.preventDefault();
-      });
-  });
+	});
+
+    $( "#category, #material, #manufacturer" )
+      .selectmenu()
+      .selectmenu( "menuWidget" )
+        .addClass( "overflow" );
+
+    $( document ).tooltip();
+
+ 	$('input#phone_number').mask("+7 (999) 999-99-99");
+
+ 	$( "#date_of_prepayment, #delivery_date, #shipment_date" ).datepicker( $.datepicker.regional[ "ru" ] );
+
+    // $("#material, #manufacturer" )
+      // .selectmenu("option", "disabled", true);
+
+});
