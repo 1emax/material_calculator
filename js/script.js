e779d58a4d4d7449d5992634dc11f05c75d93af0
1emax <1emax@mail.ru>
2015-02-04 06:51:25 +0200

    pers/legal data switch, valid. phone, datepicker, material form style

--- js/script.js
+++ js/script.js
@@ -16,9 +16,23 @@ $(function() {
 
     $( document ).tooltip();
 
- 	$('input#phone_number').mask("+7 (999) 999-99-99");
+	$('input#phone_number').mask("+7 (999) 999-99-99");
 
- 	$( "#date_of_prepayment, #delivery_date, #shipment_date" ).datepicker( $.datepicker.regional[ "ru" ] );
+	$( "#date_of_prepayment, #delivery_date, #shipment_date" ).datepicker( $.datepicker.regional[ "ru" ] );
+
+	$('input#individual, input#legal').on('click', function(e) {
+		var isLegal = $('input#legal').is(':checked');
+
+		if(isLegal === true) {
+			$('#company_name').removeClass('hide');
+		} else {
+			$('#company_name').addClass('hide');
+		}
+	});
+
+	$('.close').on('click', function(e) {
+		$(this).parents('.form_cont').remove();
+	});
 
     // $("#material, #manufacturer" )
       // .selectmenu("option", "disabled", true);
