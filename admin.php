6ee1ac06c7da746349a54c3541714aad459808cb
develop <develop@ubuntu12.vps>
2015-04-04 02:57:32 +0300

    yandex metrika

--- admin.php
+++ admin.php
@@ -18,6 +18,35 @@
     <link href="css/dashboard.css" rel="stylesheet">	
 </head>
 <body>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter28841850 = new Ya.Metrika({id:28841850,
+                    webvisor:true,
+                    clickmap:true,
+                    accurateTrackBounce:true,
+                    trackHash:true,
+                    ut:"noindex"});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/28841850?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php
 include(__DIR__ . '/index.php');
 ?>
